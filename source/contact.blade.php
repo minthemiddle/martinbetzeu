dc9e6cd426ee01e555cf888caac2524261a5e505
Martin Betz <martinmichaelbetz@gmail.com>
2019-01-05 00:31:13 +0100

    Fix: Contact wording

--- source/contact.blade.php
+++ source/contact.blade.php
@@ -11,19 +11,16 @@
 <h1>Contact</h1>
 
 <p>
-    Yes, I love to hear your stories, feedback and ideas to everything I create. I also love to network. The limited time I can put aside I like to dedicate fully to creating high quality articles and products.
-</p>
+    I love to hear your stories, feedback and ideas to everything I create. The limited time I can put aside I like to dedicate fully to creating high quality articles and products. I hence have clear priorities and do spend all of my available time right now on the following:</p>
 
-<p> I have clear priorities and will spend all of my available time on the following:</p>
-
-<ul>
+<ul class="list-reset">
     <li><a href="/products/soloheroes">SoloHeroes</a></li>
 </ul>
 
 <p>
-    If you have feedback, an offer, opportunity, or introduction that is connected to any of these projects or anything that might make my life more interesting, e-mail me: <br>
+    If you have feedback, an offer, opportunity, or introduction that is connected to my priorities or anything that might make my life more interesting, e-mail me: <br>
     <span id="cntct"></span>
 </p>
-<p>For the reasons stated above, I’ll only respond to those proposals that are a good match for my schedule and priorities.</p>
+<p>For the reasons stated above, I’ll only respond to those e-mails that are a good match for my priorities and schedule.</p>
 
 @stop
