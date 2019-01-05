90ae434c301d362e0848bb572c87a37f57e981bd
Martin Betz <martinmichaelbetz@gmail.com>
2019-01-05 17:53:53 +0100

    Fix: Replace about references in new services page

---
+++ source/about.blade.php
@@ -0,0 +1,13 @@
+@extends('_layouts.master')
+
+@push('meta')
+    <meta property="og:title" content="Services {{ $page->siteName }}" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="{{ $page->getUrl() }}"/>
+    <meta property="og:description" content="A little bit about {{ $page->siteName }}" />
+@endpush
+
+@section('body')
+    <h1>Services</h1>
+    @include('_components.about-text')
+@endsection
