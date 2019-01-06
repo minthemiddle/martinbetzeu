5c80d8d0019e7b17c21591926bd6a1b1a275e96b
Martin Betz <martinmichaelbetz@gmail.com>
2019-01-06 16:15:21 +0100

    Fix: Privacy statement wording

--- source/privacy.blade.php
+++ source/privacy.blade.php
@@ -14,9 +14,9 @@
   <li><strong>Person Responsible: </strong>I, <a href="/imprint" class="text-grey-darkest font-normal underline">Martin Betz</a>, am the sole responsible person for keeping this site GDPR compliant.</li>
   <li><strong>Data Avoidance: </strong>I respect your privacy and follow the principle of <em>data avoidance</em>.</li>
   <li><strong>Hosting: </strong>I host this static page on <a href="https://www.netlify.com/gdpr/" class="text-grey-darkest font-normal underline">Netlify</a>. They log the IP of visitors for a maximum of 30 days.</li>
-  <li><strong>Contact via email: </strong>When you contact me via a <em>email link</em>, I will get your email address. And I might use it to answer you.</li>
-  <li><strong>Newsletter (Double opt-in)</strong>: When you click my <em>newsletter</em> sign-up form, you have to confirm your email address to receive my newsletter. I will only use the address to send information about my articles, products and services. You can sign out via the link in every newsletter.</li>
-  <li><strong>Anonymous analytics:</strong> I use the self-hosted tool <a href="https://github.com/usefathom/fathom" class="text-grey-darkest font-normal underline">Fathom</a> to collect non-personally identifiable information: page visits and referrers.</li>
+  <li><strong>Contact via email: </strong>When you contact me via a <em>email link</em>, I will get your email address. I might use it to answer you.</li>
+  <li><strong>Newsletter (Double opt-in)</strong>: When you submit my <em>newsletter</em> sign-up form, you have to confirm your email address to receive my newsletters. I will only use the address to send information about my articles, products and services. You can unsubscribe via the link at the end of every newsletter.</li>
+  <li><strong>Anonymous analytics:</strong> I use the self-hosted tool <a href="https://github.com/usefathom/fathom" class="text-grey-darkest font-normal underline">Fathom</a> to collect anonymous page visits and referrers. I only log these non-personal information after you agree on the cookie banner.</li>
 </ul>
 
 @endsection
