a0c5ae55bbe2b24b188dd4f1d3f5c09cfbb364cb
Martin Betz <martinmichaelbetz@gmail.com>
2019-01-06 14:55:40 +0100

    Fix: imprint with minimal acceptable information

--- source/imprint.blade.php
+++ source/imprint.blade.php
@@ -8,5 +8,13 @@
 @endpush
 
 @section('body')
-<h1>Impressum</h1>
+<h1>Imprint</h1>
+<p>Responsible person according to German law (§5 TMG):</p>
+<ul>
+    <li>Martin Betz</li>
+    <li>Gustav-Müller-Straße 13a</li>
+    <li>10829 Berlin</li>
+    <li><span id="cntct"></span></li>
+    <li>(030) 60 26 63 04</li>
+</ul>
 @stop
