c410b570b17904977badd2d127991a8022c57782
Martin Betz <martinmichaelbetz@gmail.com>
2019-01-06 22:18:12 +0100

    Add: Confirm double-opt-in page

---
+++ source/newsletter/confirm.blade.php
@@ -0,0 +1,11 @@
+@extends('_layouts.master')
+
+@section('body')
+    <div class="flex flex-col items-center text-grey-darker mt-32">
+
+        <h2 class="text-3xl">Thanks for your interest in my newsletter.</h2>
+        <p class="text-xl">
+            I send you a mail. Please confirm your subscription by clicking the link in the mail.
+        </p>
+    </div>
+@endsection
