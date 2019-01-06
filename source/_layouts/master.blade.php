f696c6db6d3184d234dffe77d262408bdefc20a5
Martin Betz <martinmichaelbetz@gmail.com>
2019-01-06 15:23:32 +0100

    Fix: Footer link to GDPR

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -75,7 +75,7 @@
                 </li>
 
                 <li>
-                    <a href="/privacy">Privacy/Datenschutz </a>
+                    <a href="/privacy">Privacy/GDPR </a>
                 </li>
                 <li class="ml-2">
                      <a href="/imprint">Imprint</a>
