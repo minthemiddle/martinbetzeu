e70595633b9f67e5948a29ab2957e9b9ab8efe15
Martin Betz <martinmichaelbetz@gmail.com>
2019-01-06 22:42:10 +0100

    Add: Thank you page

---
+++ source/newsletter/thanks.blade.php
@@ -0,0 +1,11 @@
+@extends('_layouts.master')
+
+@section('body')
+    <div class="flex flex-col items-center text-grey-darker mt-32">
+
+        <h2 class="text-3xl">Thanks for confirming. Welcome to my newsletter!</h2>
+        <p class="text-xl">
+            You now are subscribed to my newsletter. I am curious how you like my content.
+        </p>
+    </div>
+@endsection
