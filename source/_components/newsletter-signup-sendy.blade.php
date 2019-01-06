515af857cb87f9a676544aec4baa2201ce8f276d
Martin Betz <martinmichaelbetz@gmail.com>
2019-01-06 22:11:37 +0100

    Add: sendy signup form

---
+++ source/_components/newsletter-signup-sendy.blade.php
@@ -0,0 +1,30 @@
+
+<div class="w-full justify-center lg:-mx-12 my-12 p-6 md:px-12 bg-grey-lighter border border-grey-light text-sm md:rounded shadow">
+<h3 class="text-center">Get my newsletter (6x a year)</h3>
+    <!-- Begin Sendy Signup Form -->
+    <form action="http://www.betzventures.com/sendy/subscribe" method="POST" accept-charset="utf-8">
+        <div class="mt-6 md:flex text-xl w-full">
+            <div class="md:w-2/3 flex">
+                <input type="email" name="email" id="email" class="w-full outline-none px-4 py-2 flex-1 rounded-l rounded-r-none" required placeholder="Your e-mail">
+                <div class="bg-blue-lightest py-1 px-2 rounded-r md:rounded-r-none">
+                <input type="checkbox" name="gdpr" id="gdpr" class="" required>
+                <label for="gdpr">GDPR</label>
+            </div>
+            </div>
+
+            <div class="flex-1 mt-2 md:mt-0">
+                <input type="submit" name="submit" id="submit" class="bg-blue w-full px-6 py-2 text-white rounded-r rounded-l md:rounded-l-none" value="Subscribe">
+            </div>
+        </div>
+    <div style="display:none;">
+    <label for="hp">HP</label><br/>
+    <input type="text" name="hp" id="hp"/>
+    </div>
+    <input type="hidden" name="list" value="dQyReE7eyVV7saMGnEEVpQ"/>
+    <input type="hidden" name="subform" value="yes"/>
+
+</form>
+<p class="text-center m-0 text-lg">High-quality curated resources and fresh content every second month.</p>
+<p class="text-center text-xs m-0 text-grey-dark mt-2 md:mt-0"><strong>GDPR notice:</strong> The newsletter features thoughts, summaries, articles, products and (if ever applicable) promotion for paid products</p>
+    <!--End Sendy Signup Form -->
+</div>
