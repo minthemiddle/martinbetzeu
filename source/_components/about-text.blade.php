ef99b20d2fae8cbd67211b3521bb59517dadc063
Martin Betz <martinmichaelbetz@gmail.com>
2019-09-13 06:06:29 +0200

    Add: Company workshops to about text

--- source/_components/about-text.blade.php
+++ source/_components/about-text.blade.php
@@ -1,7 +1,11 @@
 <div class="md:flex">
     <div><p class="mb-6">I am Martin. I am the CEO of <a href="https://code.design" target="_blank">Code+Design Initiative</a> – a non-profit which runs hackathons to get young people into tech.</p>
 
+        <p class="mb-6">I also run workshops for entreprises with the same goal.</p>
+
         <p class="mb-6">I also have been offering my <a href="/services">services</a> as a freelancer for over 15 years: I help my clients to define and reach their digital goals.</p>
+
+
 
         <p class="mb-6">I write <a href="/articles">articles</a> and create <a href="/products">products</a>.</p>
     </div>
