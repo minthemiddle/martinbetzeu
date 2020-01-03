80e6496c6597f09aa2f76961c65d26a4727311eb
Martin Betz <martinbetz@Mini-2018.fritz.box>
2020-01-03 12:47:31 +0100

    Cut: Newsletter privacy statement

--- source/privacy.blade.php
+++ source/privacy.blade.php
@@ -15,7 +15,6 @@
   <li><strong>Data Avoidance: </strong>I respect your privacy and follow the principle of <em>data avoidance</em>.</li>
   <li><strong>Hosting: </strong>I host this static page on <a href="https://www.netlify.com/gdpr/" class="text-grey-darkest font-normal underline">Netlify</a>. They log the IP of visitors for a maximum of 30 days.</li>
   <li><strong>Contact via email: </strong>When you contact me via a <em>email link</em>, I will get your email address. I might use it to answer you.</li>
-  <li><strong>Newsletter (Double opt-in)</strong>: When you submit my <em>newsletter</em> sign-up form, you have to confirm your email address to receive my newsletters. I will only use the address to send information about my articles, products and services. You can unsubscribe via the link at the end of every newsletter.</li>
 </ul>
 
 @endsection
