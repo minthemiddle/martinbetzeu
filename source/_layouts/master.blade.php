2ad76480413ea403073a07991ffc394cf897cead
Martin Betz <martinbetz@Mini-2018.fritz.box>
2020-01-03 12:43:10 +0100

    Cut: Tracking script

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -18,22 +18,6 @@
 
         @stack('meta')
 
-        @if ($page->production)
-            <script>
-            (function(f, a, t, h, o, m){
-                a[h]=a[h]||function(){
-                    (a[h].q=a[h].q||[]).push(arguments)
-                };
-                o=f.createElement('script'),
-                m=f.getElementsByTagName('script')[0];
-                o.async=1; o.src=t; o.id='fathom-script';
-                m.parentNode.insertBefore(o,m)
-            })(document, window, '//analytics.martinbetz.eu/tracker.js', 'fathom');
-            fathom('set', 'siteId', 'HFYWE');
-            fathom('trackPageview');
-            </script>
-        @endif
-
         <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
     </head>
 
