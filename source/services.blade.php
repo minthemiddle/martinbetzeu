aded1ace2493c995517fb320dc23fcdbb6eb50db
Martin Betz <martinbetz@Mini-2018.fritz.box>
2020-01-03 14:31:30 +0100

    Fix: Services without CEO with more specific web app

--- source/services.blade.php
+++ source/services.blade.php
@@ -11,8 +11,8 @@
     <h1>Services</h1>
     <p>I have been helping clients big and small from all over the world finding their goals and focus for digital projects for over 15 years. I am a trained designer and programmer. And I am a business graduate. I guide clients in and between both worlds in an agile iterative way.</p>
     <p>For two big German employers' associations, I developed systems to handle strikes. I made the web strategy for the big Indonesian hotel chain <em>Samali</em>. I helped German politicians such as <em>Britta Hasselmann</em> and <em>Volker Beck</em> to find their digital voice.</p>
-    <p>I have limited time for freelance projects as I'm full-time CEO of a tech foundation. Hence, I only take up projects that interest me.</p>
-    <p>If you look for support for an interesting digital project with a minimum budget of 5.000€, let's talk:</p>
+    <p>I only take up projects that interest me.</p>
+    <p>If you look for support for a challenging business web app with a minimum budget of 5.000€, let's talk:</p>
     <span id="cntct-contact"></span>
 @endsection
 
